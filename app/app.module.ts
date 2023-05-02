6fdeab76e35f45259fe8242c18e026b145723555
berat.karaaslan <berat.karaaslan@dop.com.tr>
2023-05-02 15:27:59 +0300

    fix

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-=======
 import { LocationModule } from './routes/location/location.module';
 
 @Module({
   imports: [LocationModule],
   controllers: [],
   providers: [],
->>>>>>> test
 })
 export class AppModule {}
